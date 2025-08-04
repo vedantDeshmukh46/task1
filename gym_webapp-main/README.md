5c5eb85ac84be7787d0a267312c8e6faa0d33457
vedantDeshmukh46 <131881902+vedantDeshmukh46@users.noreply.github.com>
2025-08-04 13:55:17 +0530

    Add files via upload

---
+++ gym_webapp-main/README.md
@@ -0,0 +1,2 @@
+# React Fitness Application
+
---
+++ gym_webapp-main/src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '30fa1f6b01msh1edb7746eb45372p189654jsn8b4cfc93b04f',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '30fa1f6b01msh1edb7746eb45372p189654jsn8b4cfc93b04f',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
